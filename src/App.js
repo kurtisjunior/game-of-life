be0c7ca4b7e5315d5f602a54d5c9eb55f86e3ee2
Kurtis Angell <KurtisBlow@MacBook-Pro.local>
2019-01-28 19:45:13 +0000

    first commit before logic

--- src/App.js
+++ src/App.js
@@ -1,28 +1,97 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import index from "./css/index.css";
 
 class App extends Component {
   render() {
+    return <Game />;
+  }
+}
+export default App;
+
+class Square extends React.Component {
+  render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <button className={this.props.value === 1 ? "square" : "square-two"}>
+        {this.props.value}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  state = {
+    grid: []
+  };
+
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
+
+  render() {
+    return this.state.grid.length === 0 ? (
+      <div> loading </div>
+    ) : (
+      <div className="my-grid">
+        {this.state.grid.map(col => {
+          return col.map(row => {
+            return this.renderSquare(row);
+          });
+        })}
+      </div>
+    );
+  }
+  componentDidMount = () => {
+    this.grid(30, 30);
+  };
+
+  grid = (colls, rows) => {
+    const newGrid = new Array(colls);
+    for (let i = 0; i < newGrid.length; i++) {
+      newGrid[i] = new Array(rows);
+    }
+
+    for (let i = 0; i < newGrid.length; i++) {
+      for (let j = 0; j < newGrid[i].length; j++) {
+        newGrid[i][j] = Math.floor(Math.random() * 2);
+      }
+    }
+
+    this.setState({
+      grid: newGrid
+    });
+  };
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+{
+  /* <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(0)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div> */
+}
