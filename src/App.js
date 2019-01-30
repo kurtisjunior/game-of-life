6a4ae35dc48357a9e0ceec8a591a488c936a16bd
Kurtis Angell <KurtisBlow@MacBook-Pro.local>
2019-01-30 15:24:29 +0000

    with logic

--- src/App.js
+++ src/App.js
@@ -23,25 +23,28 @@ class Board extends React.Component {
     grid: []
   };
 
-  renderSquare(i) {
-    return <Square value={i} />;
+  renderSquare(v, i) {
+    return <Square value={v} key={i} />;
   }
 
   render() {
     return this.state.grid.length === 0 ? (
       <div> loading </div>
     ) : (
-      <div className="my-grid">
-        {this.state.grid.map(col => {
-          return col.map(row => {
-            return this.renderSquare(row);
-          });
-        })}
-      </div>
+      <>
+        <div className="my-grid">
+          {this.state.grid.map(col => {
+            return col.map((v, i) => {
+              return this.renderSquare(v, i);
+            });
+          })}
+        </div>
+        <button onClick={() => this.onSubmit()}> PLAY </button>
+      </>
     );
   }
   componentDidMount = () => {
-    this.grid(30, 30);
+    this.grid(10, 10);
   };
 
   grid = (colls, rows) => {
@@ -58,6 +61,45 @@ class Board extends React.Component {
 
     this.setState({
       grid: newGrid
+    });
+  };
+
+  onSubmit = () => {
+    const { grid } = this.state;
+
+    const next = new Array(10);
+    for (let i = 0; i < next.length; i++) {
+      next[i] = new Array(10);
+    }
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+        if (
+          i === 0 ||
+          i === grid[i].length - 1 ||
+          j === 0 ||
+          j === grid[i].length - 1
+        ) {
+          next[i][j] = grid[i][j];
+        } else {
+          let neighbour = 0;
+          neighbour += grid[i - 1][j - 1];
+          neighbour += grid[i - 1][j];
+          neighbour += grid[i - 1][j + 1];
+          neighbour += grid[i][j - 1];
+          neighbour += grid[i][j + 1];
+          neighbour += grid[i + 1][j - 1];
+          neighbour += grid[i + 1][j];
+          neighbour += grid[i + 1][j + 1];
+          if (neighbour < 2 || neighbour > 3) {
+            next[i][j] = 0;
+          } else if (neighbour === 2 || neighbour === 3) {
+            next[i][j] = 1;
+          }
+        }
+      }
+    }
+    this.setState({
+      grid: next
     });
   };
 }
